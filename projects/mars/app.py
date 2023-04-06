66a07b96d11f5b80ec6ba25bb0686d0283350b8f
leedongkyu <leedong0225@icloud.com>
2023-04-06 14:45:40 +0900

    project

---
+++ projects/mars/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.gxzsir3.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/mars", methods=["POST"])
+def web_mars_post():
+    name_receive = request.form['name_give']
+    address_receive = request.form['addr_give']
+    size_receive = request.form['size_give']
+    doc = {
+        'name': name_receive,
+        'address': address_receive,
+        'size': size_receive
+    }
+    db.mars.insert_one(doc)
+    return jsonify({'msg':'주문 완료!'})
+
+
+
+    return jsonify({'msg': 'POST 연결 완료!'})
+
+@app.route("/mars", methods=["GET"])
+def web_mars_get():
+    return jsonify({'msg': 'GET 연결 완료!'})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
